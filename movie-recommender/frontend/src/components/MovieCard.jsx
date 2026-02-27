73ac028d8eeaf6b0b4870f1feeba9872e36b22d3
Preston Huss <prestonhuss42@gmail.com>
2026-02-27 07:17:00 -0600

    Remove movie-recommender from personal-finance repo

--- movie-recommender/frontend/src/components/MovieCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function MovieCard({ movie }) {
-  return (
-    <div className="movie-card">
-      <h3>{movie.title}</h3>
-      <p>Movie ID: {movie.movie_id}</p>
-      <p>Predicted Score: {movie.score.toFixed(2)}</p>
-    </div>
-  );
-}
